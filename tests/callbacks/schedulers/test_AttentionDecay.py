b22523e16e6a71da897f84911f900f5ad32b5c50
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2022-12-14 16:07:55 +0100

    added several lr schedulers and tests

---
+++ tests/callbacks/schedulers/test_AttentionDecay.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+import tensorflow as tf
+
+
+chunk_size = int(1e5)
+
+X = np.c_[
+  np.random.uniform(-1, 1, size=chunk_size),
+  np.random.normal(0, 1, size=chunk_size),
+  np.random.exponential(5, size=chunk_size)
+]
+Y = np.tanh(X[:,0]) + 2 * X[:,1] * X[:,2]
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.InputLayer(input_shape=(3,)))
+for units in [16, 16, 16]:
+  model.add(tf.keras.layers.Dense(units, activation="relu"))
+model.add(tf.keras.layers.Dense(1))
+
+
+@pytest.fixture
+def scheduler():
+  from calotron.callbacks.schedulers import AttentionDecay
+  sched = AttentionDecay(d_model=512, warmup_steps=500)
+  return sched
+
+
+###########################################################################
+
+
+def test_sched_configuration(scheduler):
+  from calotron.callbacks.schedulers import AttentionDecay
+  assert isinstance(scheduler, AttentionDecay)
+  assert isinstance(scheduler.d_model, int)
+  assert isinstance(scheduler.warmup_steps, int)
+
+
+def test_sched_use(scheduler):
+  adam = tf.keras.optimizers.Adam(learning_rate=0.001)
+  mse = tf.keras.losses.MeanSquaredError()
+  model.compile(optimizer=adam, loss=mse)
+  history = model.fit(X, Y, batch_size=512, epochs=10, callbacks=[scheduler])
+  assert isinstance(scheduler._dtype, np.dtype)
+  last_lr = float(history.history["lr"][-1])
+  assert  last_lr < 0.001
---
+++ tests/callbacks/schedulers/test_InverseTimeDecay.py
@@ -0,0 +1,50 @@
+import pytest
+import numpy as np
+import tensorflow as tf
+
+
+chunk_size = int(1e5)
+
+X = np.c_[
+  np.random.uniform(-1, 1, size=chunk_size),
+  np.random.normal(0, 1, size=chunk_size),
+  np.random.exponential(5, size=chunk_size)
+]
+Y = np.tanh(X[:,0]) + 2 * X[:,1] * X[:,2]
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.InputLayer(input_shape=(3,)))
+for units in [16, 16, 16]:
+  model.add(tf.keras.layers.Dense(units, activation="relu"))
+model.add(tf.keras.layers.Dense(1))
+
+
+@pytest.fixture
+def scheduler(staircase=False):
+  from calotron.callbacks.schedulers import InverseTimeDecay
+  sched = InverseTimeDecay(decay_rate=0.9, decay_steps=1000, staircase=staircase)
+  return sched
+
+
+###########################################################################
+
+
+def test_sched_configuration(scheduler):
+  from calotron.callbacks.schedulers import InverseTimeDecay
+  assert isinstance(scheduler, InverseTimeDecay)
+  assert isinstance(scheduler.decay_rate, float)
+  assert isinstance(scheduler.decay_steps, int)
+  assert isinstance(scheduler.staircase, bool)
+
+
+@pytest.mark.parametrize("staircase", [False, True])
+def test_sched_use(staircase):
+  from calotron.callbacks.schedulers import InverseTimeDecay
+  sched = InverseTimeDecay(decay_rate=0.9, decay_steps=1000, staircase=staircase)
+  adam = tf.keras.optimizers.Adam(learning_rate=0.001)
+  mse = tf.keras.losses.MeanSquaredError()
+  model.compile(optimizer=adam, loss=mse)
+  history = model.fit(X, Y, batch_size=512, epochs=10, callbacks=[sched])
+  assert isinstance(sched._dtype, np.dtype)
+  last_lr = float(history.history["lr"][-1])
+  assert  last_lr < 0.001
