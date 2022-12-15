4845dba73898f5f41474e8bb17530dc073794fb5
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2022-12-15 17:39:22 +0100

    added test for BaseAttention

---
+++ tests/layers/Attention/test_BaseAttention.py
@@ -0,0 +1,17 @@
+import pytest
+import tensorflow as tf
+
+
+@pytest.fixture
+def layer():
+  from calotron.layers.Attention import BaseAttention
+  att = BaseAttention(num_heads=8, key_dim=64)
+  return att
+
+
+###########################################################################
+
+
+def test_layer_configuration(layer):
+  from calotron.layers.Attention import BaseAttention
+  assert isinstance(layer, BaseAttention)
