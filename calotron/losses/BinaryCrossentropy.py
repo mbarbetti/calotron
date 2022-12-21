fixed BCE computation and updated tests

calotron/losses/BinaryCrossentropy.py
@@ -18,7 +18,10 @@ class BinaryCrossentropy(BaseLoss):
                         name=name)
   
   def discriminator_loss(self, y_true, y_pred, **kwargs):
-    return -self._loss(y_true, y_pred, **kwargs)   # loss maximization
+    loss_real = self(tf.ones_like(y_true), y_true, **kwargs)
+    loss_fake = self(tf.zeros_like(y_pred), y_pred, **kwargs)
+    return loss_real + loss_fake
 
   def transformer_loss(self, y_true, y_pred, **kwargs):
-    return self._loss(y_true, y_pred, **kwargs)   # loss minimization
+    loss_fake = self(tf.ones_like(y_pred), y_pred, **kwargs)
+    return loss_fake

calotron/losses/JSDivergence.py
@@ -8,13 +8,16 @@ class JSDivergence(BaseLoss):
     super().__init__(name)
     self._kl_div = TF_KLDivergence(reduction=reduction, name=name)
   
-  def discriminator_loss(self, y_true, y_pred, **kwargs):
-    return -self.transformer_loss(y_true, y_pred, **kwargs)   # loss maximization
-
-  def transformer_loss(self, y_true, y_pred, **kwargs):
+  def __call__(self, y_true, y_pred, **kwargs):
     dtype = self._kl_div(y_true, y_pred).dtype
     y_true = tf.cast(y_true, dtype)
     y_pred = tf.cast(y_pred, dtype)
     loss = 0.5 * self._kl_div(y_true, 0.5 * (y_true + y_pred), **kwargs) + \
            0.5 * self._kl_div(y_pred, 0.5 * (y_true + y_pred), **kwargs)
-    return loss   # loss minimization
+    return loss
+
+  def discriminator_loss(self, y_true, y_pred, **kwargs):
+    return -self(y_true, y_pred, **kwargs)   # divergence maximization
+
+  def transformer_loss(self, y_true, y_pred, **kwargs):
+    return self(y_true, y_pred, **kwargs)   # divergence minimization

tests/losses/test_BinaryCrossentropy_loss.py
@@ -3,8 +3,10 @@ import numpy as np
 import tensorflow as tf
 
 
-y_true = [[0, 1], [0, 0]]
-y_pred = [[-18.6, 0.51], [2.94, -12.8]]
+np.random.seed(42)
+chunk_size = int(5e4)
+y_true = np.random.uniform(0.5, 1.0, size=(chunk_size,))
+y_pred = np.random.uniform(0.2, 0.8, size=(chunk_size,))
 
 
 @pytest.fixture
@@ -37,7 +39,7 @@ def test_loss_use(from_logits):
                             name="bce_loss")
   out1 = loss.discriminator_loss(y_true, y_pred).numpy()
   out2 = loss.transformer_loss(y_true, y_pred).numpy()
-  assert out1 == -out2
+  assert out1 > out2
 
 
 @pytest.mark.parametrize("reduction",
@@ -45,18 +47,17 @@ def test_loss_use(from_logits):
                           tf.keras.losses.Reduction.NONE])
 def test_loss_reduction(reduction):
   from calotron.losses import BinaryCrossentropy
-  loss = BinaryCrossentropy(from_logits=True,
+  loss = BinaryCrossentropy(from_logits=False,
                             label_smoothing=0.0,
                             axis=-1,
                             reduction=reduction,
                             name="bce_loss")
   out1 = loss.discriminator_loss(y_true, y_pred).numpy()
   out2 = loss.transformer_loss(y_true, y_pred).numpy()
-  assert np.all(np.equal(out1, -out2))
 
 
 def test_loss_kargs(loss):
-  w = [0.8, 0.2]
-  out1 = loss.discriminator_loss(y_true, y_pred, sample_weight=w).numpy()
-  out2 = loss.transformer_loss(y_true, y_pred, sample_weight=w).numpy()
-  assert out1 == - out2
+  w = np.random.uniform(0.0, 1.0, size=(chunk_size,)) > 0.5
+  out1 = loss.discriminator_loss(y_true, y_pred, sample_weight=w[None,:]).numpy()
+  out2 = loss.transformer_loss(y_true, y_pred, sample_weight=w[None,:]).numpy()
+  assert out1 > out2