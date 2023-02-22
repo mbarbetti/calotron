3f72ef0a95b2729609c1f70902ae9069a354d613
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-02-22 18:29:07 +0100

    minor changes

--- scripts/initial_setup.py
+++ scripts/initial_setup.py
@@ -7,20 +7,20 @@ import yaml
 # +--------------------------+
 
 with open("config/directories.yml") as file:
-  config = yaml.full_load(file)
+  config_dir = yaml.full_load(file)
 
-data_dir = config["data_dir"]
+data_dir = config_dir["data_dir"]
 if not os.path.exists (data_dir):
   os.makedirs(data_dir)
 
-export_dir = config["export_dir"]
+export_dir = config_dir["export_dir"]
 if not os.path.exists (export_dir):
   os.makedirs(export_dir)
 
-images_dir = config["images_dir"]
+images_dir = config_dir["images_dir"]
 if not os.path.exists (images_dir):
   os.makedirs(images_dir)
 
-report_dir = config["report_dir"]
+report_dir = config_dir["report_dir"]
 if not os.path.exists (report_dir):
   os.makedirs(report_dir)
