927c8c553d6697ad46ea4a7044b4798fef4a6b18
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-03-30 18:20:30 +0200

    added photon-to-cluster plot

--- scripts/utils.py
+++ scripts/utils.py
@@ -16,16 +16,20 @@ def learning_curves(
 ) -> None:
     if scale_curves:
         id_min = np.argmin(
-            [np.mean(np.array(history.history[k])[start_epoch:]) for k in keys]
+            [np.abs(np.mean(np.array(history[k])[start_epoch:])) for k in keys]
         )
         scales = [
-            np.array(history.history[k])[start_epoch:]
-            / np.array(history.history[keys[id_min]])[start_epoch:]
+            np.abs(
+                np.array(history[k])[start_epoch:]
+                / np.array(history[keys[id_min]])[start_epoch:]
+            )
             for k in keys
         ]
         scales = np.around(np.mean(scales, axis=-1))
         for i in range(len(labels)):
             labels[i] += f" [x {1/scales[i]:.1f}]"
+    else:
+        scales = [1.0 for _ in keys]
 
     if colors is None:
         colors = [None for _ in range(len(keys))]
@@ -42,8 +46,8 @@ def learning_curves(
     plt.xlabel("Training epochs", fontsize=12)
     plt.ylabel("Loss", fontsize=12)
     for i, (k, l, c) in enumerate(zip(keys, labels, colors)):
-        num_epochs = np.arange(len(history.history[k]))[start_epoch:]
-        loss = np.array(history.history[k])[start_epoch:] / scales[i]
+        num_epochs = np.arange(len(history[k]))[start_epoch:]
+        loss = np.array(history[k])[start_epoch:] / scales[i]
         plt.plot(num_epochs, loss, lw=1.5, color=c, label=l)
     plt.legend(loc=legend_loc, fontsize=10)
     if save_figure:
@@ -78,8 +82,8 @@ def learn_rate_scheduling(
     plt.xlabel("Training epochs", fontsize=12)
     plt.ylabel("Learning rate", fontsize=12)
     for k, l, c in zip(keys, labels, colors):
-        num_epochs = np.arange(len(history.history[k]))[start_epoch:]
-        lr = np.array(history.history[k])[start_epoch:]
+        num_epochs = np.arange(len(history[k]))[start_epoch:]
+        lr = np.array(history[k])[start_epoch:]
         plt.plot(num_epochs, lr, lw=1.5, color=c, label=l)
     plt.legend(loc=legend_loc, fontsize=10)
     plt.yscale("log")
@@ -122,8 +126,8 @@ def metric_curves(
     plt.xlabel("Training epochs", fontsize=12)
     plt.ylabel(ylabel, fontsize=12)
     for k, l, c in zip(keys, labels, colors):
-        num_epochs = np.arange(len(history.history[k]))[start_epoch:]
-        metric = np.array(history.history[k])[start_epoch:]
+        num_epochs = np.arange(len(history[k]))[start_epoch:]
+        metric = np.array(history[k])[start_epoch:]
         plt.plot(num_epochs, metric, lw=1.5, color=c, label=l, zorder=zorder)
         zorder -= 1
     plt.legend(loc=legend_loc, fontsize=10)
@@ -170,16 +174,16 @@ def validation_histogram(
 
 def calorimeter_deposits(
     report,
-    ref_coords,
-    gen_coords,
+    ref_xy,
+    gen_xy,
     ref_energy=None,
     gen_energy=None,
     scaler=None,
     save_figure=False,
     export_fname="./images/calo-deposits.png",
 ) -> None:
-    x_ref, y_ref = ref_coords
-    x_gen, y_gen = gen_coords
+    x_ref, y_ref = ref_xy
+    x_gen, y_gen = gen_xy
 
     if scaler is not None:
         ref_energy = scaler.inverse_transform(np.c_[ref_energy]).flatten()
@@ -188,7 +192,7 @@ def calorimeter_deposits(
     x_bins = np.linspace(x_ref.min(), x_ref.max(), 101)
     y_bins = np.linspace(y_ref.min(), y_ref.max(), 101)
 
-    plt.figure(figsize=(16, 5), dpi=300)
+    plt.figure(figsize=(18, 5), dpi=300)
     plt.subplot(1, 2, 1)
     plt.title("Training data", fontsize=14)
     plt.xlabel("$x$ coordinate", fontsize=12)
@@ -201,6 +205,7 @@ def calorimeter_deposits(
         cmin=0,
         cmap="gist_heat",
     )
+    plt.colorbar()
     plt.subplot(1, 2, 2)
     plt.title("Calotron output", fontsize=14)
     plt.xlabel("$x$ coordinate", fontsize=12)
@@ -213,6 +218,7 @@ def calorimeter_deposits(
         cmin=0,
         cmap="gist_heat",
     )
+    plt.colorbar()
     if save_figure:
         plt.savefig(export_fname)
     report.add_figure(options="width=95%")
@@ -221,9 +227,9 @@ def calorimeter_deposits(
 
 def event_example(
     report,
-    photon_coords,
-    cluster_coords,
-    output_coords,
+    photon_xy,
+    cluster_xy,
+    output_xy,
     photon_energy=None,
     cluster_energy=None,
     output_energy=None,
@@ -232,9 +238,9 @@ def event_example(
     save_figure=False,
     export_fname="./images/evt-example.png",
 ) -> None:
-    x_photon, y_photon = photon_coords
-    x_cluster, y_cluster = cluster_coords
-    x_output, y_output = output_coords
+    x_photon, y_photon = photon_xy
+    x_cluster, y_cluster = cluster_xy
+    x_output, y_output = output_xy
 
     if (
         (photon_energy is not None)
@@ -317,17 +323,70 @@ def energy_sequences(
         ref_energy = ref_energy.reshape(batch_size, length)
         gen_energy = gen_energy.reshape(batch_size, length)
 
-    plt.figure(figsize=(16, 10), dpi=300)
+    plt.figure(figsize=(18, 10), dpi=300)
     plt.subplot(1, 2, 1)
     plt.title("Reconstructed clusters", fontsize=14)
     plt.xlabel("Preprocessed energy deposits [a.u.]", fontsize=12)
     plt.ylabel("Events", fontsize=12)
-    plt.imshow(ref_energy, aspect="auto", cmap="gist_heat")
+    plt.imshow(ref_energy, aspect="auto", vmin=0.0, vmax=1.0, cmap="gist_heat")
+    plt.colorbar()
     plt.subplot(1, 2, 2)
     plt.title("Calotron output", fontsize=14)
     plt.xlabel("Preprocessed energy deposits [a.u.]", fontsize=12)
     plt.ylabel("Events", fontsize=12)
-    plt.imshow(gen_energy, aspect="auto", cmap="gist_heat")
+    plt.imshow(gen_energy, aspect="auto", vmin=0.0, vmax=1.0, cmap="gist_heat")
+    plt.colorbar()
+    if save_figure:
+        plt.savefig(export_fname)
+    report.add_figure(options="width=95%")
+    plt.close()
+
+
+def photon2cluster_corr(
+    report,
+    photon_energy,
+    cluster_energy,
+    output_energy,
+    photon_scaler=None,
+    cluster_scaler=None,
+    save_figure=False,
+    export_fname="./images/gamma2calo-corr.png",
+) -> None:
+    if photon_scaler is not None:
+        photon_energy = photon_scaler.inverse_transform(np.c_[photon_energy]).flatten()
+    if cluster_scaler is not None:
+        cluster_energy = cluster_scaler.inverse_transform(
+            np.c_[cluster_energy]
+        ).flatten()
+        output_energy = cluster_scaler.inverse_transform(np.c_[output_energy]).flatten()
+
+    energies = [cluster_energy, output_energy]
+    titles = ["Photon-to-cluster correlations", "Photon-to-output correlations"]
+    ylabels = [
+        "Cluster preprocessed energy [a.u.]",
+        "Output preprocessed energy [a.u.]",
+    ]
+
+    plt.figure(figsize=(18, 5), dpi=300)
+    for i, (energy, title, ylabel) in enumerate(zip(energies, titles, ylabels)):
+        h, x_edges, y_edges = np.histogram2d(
+            photon_energy, energy, bins=np.linspace(0, 1, 76)
+        )
+        nonzero_x, nonzero_y = np.nonzero(h)
+        colors = np.ones_like(photon_energy) / np.sum(h)
+        for id_x, id_y in zip(nonzero_x, nonzero_y):
+            colors[
+                ((photon_energy >= x_edges[id_x]) & (photon_energy < x_edges[id_x + 1]))
+                & ((energy >= y_edges[id_y]) & (energy < y_edges[id_y + 1]))
+            ] *= h[id_x, id_y]
+        plt.subplot(1, 2, i + 1)
+        plt.title(title, fontsize=14)
+        plt.xlabel("Photon preprocessed energy [a.u.]", fontsize=12)
+        plt.ylabel(ylabel, fontsize=12)
+        plt.scatter(photon_energy, energy, s=1, c=colors, cmap="gist_heat")
+        plt.xlim(0.0, 1.0)
+        plt.ylim(0.0, 1.0)
+        plt.colorbar()
     if save_figure:
         plt.savefig(export_fname)
     report.add_figure(options="width=95%")
