8b5ada3e9bd7f11621bc3615551ef20962daed29
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-05-30 21:29:35 +0200

    removed aux from calotron definition

--- src/calotron/models/Calotron.py
+++ src/calotron/models/Calotron.py
@@ -1,15 +1,12 @@
 import tensorflow as tf
 
-from calotron.models.auxiliaries import AuxClassifier
 from calotron.models.discriminators import BaseDiscriminator
 from calotron.models.transformers import BaseTransformer
 from calotron.utils.checks import checkLoss, checkMetrics, checkOptimizer
 
 
 class Calotron(tf.keras.Model):
-    def __init__(
-        self, transformer, discriminator, aux_classifier=None, name=None, dtype=None
-    ) -> None:
+    def __init__(self, transformer, discriminator, name=None, dtype=None) -> None:
         super().__init__(name=name, dtype=dtype)
 
         # Transformer
@@ -28,32 +25,17 @@ class Calotron(tf.keras.Model):
             )
         self._discriminator = discriminator
 
-        # Auxiliary Classifier
-        if aux_classifier is not None:
-            if not isinstance(aux_classifier, AuxClassifier):
-                raise TypeError(
-                    f"`aux_classifier` should be a calotron's `AuxClassifier`, "
-                    f"instead {type(aux_classifier)} passed"
-                )
-        self._aux_classifier = aux_classifier
-
     def call(self, inputs) -> tuple:
         source, target = inputs
         output = self._transformer((source, target))
         d_output_true = self._discriminator((source, target))
         d_output_pred = self._discriminator((source, output))
-        if self._aux_classifier is None:
-            return output, d_output_true, d_output_pred
-        else:
-            aux_output = self._aux_classifier(source)
-            return output, d_output_true, d_output_pred, aux_output
+        return output, d_output_true, d_output_pred
 
     def summary(self, **kwargs) -> None:
         print("_" * 65)
         self._transformer.summary(**kwargs)
         self._discriminator.summary(**kwargs)
-        if self._aux_classifier is not None:
-            self._aux_classifier.summary(**kwargs)
 
     def compile(
         self,
@@ -61,30 +43,20 @@ class Calotron(tf.keras.Model):
         metrics=None,
         transformer_optimizer="rmsprop",
         discriminator_optimizer="rmsprop",
-        aux_classifier_optimizer=None,
         transformer_upds_per_batch=1,
         discriminator_upds_per_batch=1,
-        aux_classifier_upds_per_batch=None,
     ) -> None:
-        super().compile()
+        super().compile(weighted_metrics=[])
 
         # Loss metrics
         self._loss = checkLoss(loss)
         self._t_loss = tf.keras.metrics.Mean(name="t_loss")
         self._d_loss = tf.keras.metrics.Mean(name="d_loss")
-        if self._aux_classifier is not None:
-            self._a_loss = tf.keras.metrics.Mean(name="a_loss")
-        else:
-            self._a_loss = None
         self._metrics = checkMetrics(metrics)
 
         # Optimizers
         self._t_opt = checkOptimizer(transformer_optimizer)
         self._d_opt = checkOptimizer(discriminator_optimizer)
-        if self._aux_classifier is not None:
-            self._a_opt = checkOptimizer(aux_classifier_optimizer)
-        else:
-            self._a_opt = None
 
         # Transformer updates per batch
         assert isinstance(transformer_upds_per_batch, (int, float))
@@ -96,24 +68,12 @@ class Calotron(tf.keras.Model):
         assert discriminator_upds_per_batch >= 1
         self._d_upds_per_batch = int(discriminator_upds_per_batch)
 
-        # Auxiliary Classifier updates per batch
-        if self._aux_classifier is not None:
-            assert isinstance(aux_classifier_upds_per_batch, (int, float))
-            assert aux_classifier_upds_per_batch >= 1
-            self._a_upds_per_batch = int(aux_classifier_upds_per_batch)
-        else:
-            self._a_upds_per_batch = None
-
     def train_step(self, data) -> dict:
         if len(data) == 3:
             source, target, sample_weight = data
         else:
             source, target = data
             sample_weight = None
-
-        if self._aux_classifier is not None:
-            for _ in range(self._a_upds_per_batch):
-                self._a_train_step(source, target, sample_weight)
 
         for _ in range(self._d_upds_per_batch):
             self._d_train_step(source, target, sample_weight)
@@ -123,34 +83,26 @@ class Calotron(tf.keras.Model):
         train_dict = dict()
         if self._metrics is not None:
             output = self._transformer((source, target), training=False)
-            y_pred = self._discriminator((source, output), training=False)
-            y_true = self._discriminator((source, target), training=False)
+            y_pred = self._discriminator(
+                (source, output), filter=sample_weight, training=False
+            )
+            y_true = self._discriminator(
+                (source, target), filter=sample_weight, training=False
+            )
             for metric in self._metrics:
                 metric.update_state(
                     y_true=y_true, y_pred=y_pred, sample_weight=sample_weight
                 )
                 train_dict.update({metric.name: metric.result()})
 
-        if self._aux_classifier is not None:
-            train_dict.update(
-                {
-                    "a_loss": self._a_loss.result(),
-                    "t_loss": self._t_loss.result(),
-                    "d_loss": self._d_loss.result(),
-                    "a_lr": self._a_opt.learning_rate,
-                    "t_lr": self._t_opt.learning_rate,
-                    "d_lr": self._d_opt.learning_rate,
-                }
-            )
-        else:
-            train_dict.update(
-                {
-                    "t_loss": self._t_loss.result(),
-                    "d_loss": self._d_loss.result(),
-                    "t_lr": self._t_opt.learning_rate,
-                    "d_lr": self._d_opt.learning_rate,
-                }
-            )
+        train_dict.update(
+            {
+                "t_loss": self._t_loss.result(),
+                "d_loss": self._d_loss.result(),
+                "t_lr": self._t_opt.learning_rate,
+                "d_lr": self._d_opt.learning_rate,
+            }
+        )
         return train_dict
 
     def _t_train_step(self, source, target, sample_weight) -> None:
@@ -183,20 +135,6 @@ class Calotron(tf.keras.Model):
         self._d_opt.apply_gradients(zip(gradients, trainable_vars))
         self._d_loss.update_state(loss)
 
-    def _a_train_step(self, source, target, sample_weight) -> None:
-        with tf.GradientTape() as tape:
-            loss = self._loss.aux_classifier_loss(
-                aux_classifier=self._aux_classifier,
-                source=source,
-                target=target,
-                sample_weight=sample_weight,
-                training=True,
-            )
-        trainable_vars = self._aux_classifier.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-        self._a_opt.apply_gradients(zip(gradients, trainable_vars))
-        self._a_loss.update_state(loss)
-
     def test_step(self, data) -> dict:
         if len(data) == 3:
             source, target, sample_weight = data
@@ -224,47 +162,29 @@ class Calotron(tf.keras.Model):
         )
         self._d_loss.update_state(d_loss)
 
-        if self._aux_classifier is not None:
-            a_loss = self._loss.aux_classifier_loss(
-                aux_classifier=self._aux_classifier,
-                source=source,
-                target=target,
-                sample_weight=sample_weight,
-                training=False,
-            )
-            self._a_loss.update_state(a_loss)
-
         train_dict = dict()
         if self._metrics is not None:
             output = self._transformer((source, target), training=False)
-            y_pred = self._discriminator((source, output), training=False)
-            y_true = self._discriminator((source, target), training=False)
+            y_pred = self._discriminator(
+                (source, output), filter=sample_weight, training=False
+            )
+            y_true = self._discriminator(
+                (source, target), filter=sample_weight, training=False
+            )
             for metric in self._metrics:
                 metric.update_state(
                     y_true=y_true, y_pred=y_pred, sample_weight=sample_weight
                 )
                 train_dict.update({metric.name: metric.result()})
 
-        if self._aux_classifier is not None:
-            train_dict.update(
-                {
-                    "a_loss": self._a_loss.result(),
-                    "t_loss": self._t_loss.result(),
-                    "d_loss": self._d_loss.result(),
-                    "a_lr": self._a_opt.learning_rate,
-                    "t_lr": self._t_opt.learning_rate,
-                    "d_lr": self._d_opt.learning_rate,
-                }
-            )
-        else:
-            train_dict.update(
-                {
-                    "t_loss": self._t_loss.result(),
-                    "d_loss": self._d_loss.result(),
-                    "t_lr": self._t_opt.learning_rate,
-                    "d_lr": self._d_opt.learning_rate,
-                }
-            )
+        train_dict.update(
+            {
+                "t_loss": self._t_loss.result(),
+                "d_loss": self._d_loss.result(),
+                "t_lr": self._t_opt.learning_rate,
+                "d_lr": self._d_opt.learning_rate,
+            }
+        )
         return train_dict
 
     def get_start_token(self, target) -> tf.Tensor:
@@ -277,10 +197,6 @@ class Calotron(tf.keras.Model):
     @property
     def discriminator(self) -> BaseDiscriminator:
         return self._discriminator
-
-    @property
-    def aux_classifier(self) -> AuxClassifier:
-        return self._aux_classifier
 
     @property
     def metrics(self) -> list:
@@ -298,17 +214,9 @@ class Calotron(tf.keras.Model):
         return self._d_opt
 
     @property
-    def aux_classifier_optimizer(self) -> tf.keras.optimizers.Optimizer:
-        return self._a_opt
-
-    @property
     def transformer_upds_per_batch(self) -> int:
         return self._t_upds_per_batch
 
     @property
     def discriminator_upds_per_batch(self) -> int:
         return self._d_upds_per_batch
-
-    @property
-    def aux_classifier_upds_per_batch(self) -> int:
-        return self._a_upds_per_batch
--- tests/models/test_Calotron.py
+++ tests/models/test_Calotron.py
@@ -6,12 +6,12 @@ CHUNK_SIZE = int(1e4)
 
 source = tf.random.normal(shape=(CHUNK_SIZE, 8, 5))
 target = tf.random.normal(shape=(CHUNK_SIZE, 4, 3))
+weight = tf.random.uniform(shape=(CHUNK_SIZE, target.shape[1]))
 
 
 @pytest.fixture
 def model():
     from calotron.models import Calotron
-    from calotron.models.auxiliaries import AuxClassifier
     from calotron.models.discriminators import Discriminator
     from calotron.models.transformers import Transformer
 
@@ -41,14 +41,7 @@ def model():
         dropout_rate=0.1,
     )
 
-    aux = AuxClassifier(
-        transformer=transf,
-        output_depth=1,
-        output_activation="sigmoid",
-        dropout_rate=0.1,
-    )
-
-    calo = Calotron(transformer=transf, discriminator=disc, aux_classifier=aux)
+    calo = Calotron(transformer=transf, discriminator=disc)
     return calo
 
 
@@ -57,19 +50,17 @@ def model():
 
 def test_model_configuration(model):
     from calotron.models import Calotron
-    from calotron.models.auxiliaries import AuxClassifier
     from calotron.models.discriminators import Discriminator
     from calotron.models.transformers import Transformer
 
     assert isinstance(model, Calotron)
     assert isinstance(model.transformer, Transformer)
     assert isinstance(model.discriminator, Discriminator)
-    assert isinstance(model.aux_classifier, AuxClassifier)
 
 
 def test_model_use(model):
     outputs = model((source, target))
-    t_output, d_output_true, d_output_pred, a_output = outputs
+    t_output, d_output_true, d_output_pred= outputs
     model.summary()
 
     test_t_shape = list(target.shape)
@@ -81,88 +72,88 @@ def test_model_use(model):
     assert d_output_true.shape == tuple(test_d_shape)
     assert d_output_pred.shape == tuple(test_d_shape)
 
-    test_a_shape = list(source.shape)
-    test_a_shape[-1] = model.aux_classifier.output_depth
-    assert a_output.shape == tuple(test_a_shape)
-
 
 @pytest.mark.parametrize("metrics", [["bce"], None])
 def test_model_compilation(model, metrics):
-    from calotron.losses import PhotonClusterMatch
+    from calotron.losses import MeanSquaredError
 
-    loss = PhotonClusterMatch(
-        alpha=0.1, max_match_distance=0.005, adversarial_metric="binary-crossentropy"
+    loss = MeanSquaredError(
+        warmup_energy=0.0,
+        alpha=0.1,
+        adversarial_metric="binary-crossentropy",
     )
     t_opt = RMSprop(learning_rate=0.001)
     d_opt = RMSprop(learning_rate=0.001)
-    a_opt = RMSprop(learning_rate=0.001)
     model.compile(
         loss=loss,
         metrics=metrics,
         transformer_optimizer=t_opt,
         discriminator_optimizer=d_opt,
-        aux_classifier_optimizer=a_opt,
         transformer_upds_per_batch=1,
         discriminator_upds_per_batch=1,
-        aux_classifier_upds_per_batch=1,
     )
     assert isinstance(model.metrics, list)
     assert isinstance(model.transformer_optimizer, Optimizer)
     assert isinstance(model.discriminator_optimizer, Optimizer)
-    assert isinstance(model.aux_classifier_optimizer, Optimizer)
     assert isinstance(model.transformer_upds_per_batch, int)
     assert isinstance(model.discriminator_upds_per_batch, int)
-    assert isinstance(model.aux_classifier_upds_per_batch, int)
 
 
 @pytest.mark.parametrize(
     "adversarial_metrics", ["binary-crossentropy", "wasserstein-distance"]
 )
-def test_model_train(model, adversarial_metrics):
+@pytest.mark.parametrize("sample_weight", [weight, None])
+def test_model_train(model, adversarial_metrics, sample_weight):
+    if sample_weight is not None:
+        slices = (source, target, weight)
+    else:
+        slices = (source, target)
     dataset = (
-        tf.data.Dataset.from_tensor_slices((source, target))
+        tf.data.Dataset.from_tensor_slices(slices)
         .batch(batch_size=512, drop_remainder=True)
         .cache()
         .prefetch(tf.data.AUTOTUNE)
     )
-    from calotron.losses import PhotonClusterMatch
+    from calotron.losses import MeanSquaredError
 
-    loss = PhotonClusterMatch(
-        alpha=0.1, max_match_distance=0.005, adversarial_metric=adversarial_metrics
+    loss = MeanSquaredError(
+        warmup_energy=0.0,
+        alpha=0.1,
+        adversarial_metric=adversarial_metrics,
     )
     t_opt = RMSprop(learning_rate=0.001)
     d_opt = RMSprop(learning_rate=0.001)
-    a_opt = RMSprop(learning_rate=0.001)
     model.compile(
         loss=loss,
         metrics=None,
         transformer_optimizer=t_opt,
         discriminator_optimizer=d_opt,
-        aux_classifier_optimizer=a_opt,
         transformer_upds_per_batch=1,
         discriminator_upds_per_batch=1,
-        aux_classifier_upds_per_batch=1,
     )
     model.fit(dataset, epochs=2)
 
 
-def test_model_eval(model):
-    from calotron.losses import PhotonClusterMatch
+@pytest.mark.parametrize("sample_weight", [weight, None])
+def test_model_eval(model, sample_weight):
+    from calotron.losses import MeanSquaredError
 
-    loss = PhotonClusterMatch(
-        alpha=0.1, max_match_distance=0.005, adversarial_metric="binary-crossentropy"
+    loss = MeanSquaredError(
+        warmup_energy=0.0,
+        alpha=0.1,
+        adversarial_metric="binary-crossentropy",
     )
     t_opt = RMSprop(learning_rate=0.001)
     d_opt = RMSprop(learning_rate=0.001)
-    a_opt = RMSprop(learning_rate=0.001)
     model.compile(
         loss=loss,
         metrics=None,
         transformer_optimizer=t_opt,
         discriminator_optimizer=d_opt,
-        aux_classifier_optimizer=a_opt,
         transformer_upds_per_batch=1,
         discriminator_upds_per_batch=1,
-        aux_classifier_upds_per_batch=1,
     )
-    model.evaluate(source, target)
+    if sample_weight is not None:
+        model.evaluate(source, target, sample_weight=weight)
+    else:
+        model.evaluate(source, target)
